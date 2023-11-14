04a27477e197eb46a53fee7f74f9cffc5a575473
Eddie <eeeeddie517@gmail.com>
2023-11-14 22:53:49 +0800

    youbike_v2

--- components/layout/default-layout/my-footer.js
+++
@@ -1,9 +0,0 @@
-export default function MyFooter() {
-  return (
-    <footer className="footer mt-auto py-3 bg-light">
-      <div className="container">
-        <span className="text-muted">台灣 © 2023 NextJS, Inc. 版權所有</span>
-      </div>
-    </footer>
-  )
-}
